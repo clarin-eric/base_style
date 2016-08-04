fed96fa020a529355b2c5a02536fbd96c94be5cf
Sander Maijers <S.N.Maijers@gmail.com>
2016-08-04 17:29:24 +0200

    Use path name, `$_` did not work
    In non-interactive mode `$_` evaluates to `$0` it appears.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -- 'bootstrap/'
-cd -- "$_" || return
+cd -- 'bootstrap/' || return
 curl --fail --location --show-error --silent --tlsv1.2 -- \
     "$BOOTSTRAP_ARTIFACT_URL" | \
     tar -f - -p --strip-components=1 -x -z
