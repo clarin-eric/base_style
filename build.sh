dd9284aa38fd440e3f063a475d1c4e14115ade80
Sander Maijers <S.N.Maijers@gmail.com>
2016-08-04 17:36:49 +0200

    Do not use `curl` with `--tlsv1.2`
    Travis CI uses an outdated OS release (Ubuntu 12.04) and its `curl` does not
    support  this CLI param.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 mkdir -- 'bootstrap/'
 cd -- 'bootstrap/' || return
-curl --fail --location --show-error --silent --tlsv1.2 -- \
+curl --fail --location --show-error --silent --tlsv1 -- \
     "$BOOTSTRAP_ARTIFACT_URL" | \
     tar -f - -p --strip-components=1 -x -z
 cd -- '-' || return
