df9f837ce0e3549eeec3c4d588b5b3b90bd0a683
Sander Maijers <S.N.Maijers@gmail.com>
2016-08-04 17:51:11 +0200

    Rename variable

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 mkdir -- 'bootstrap/'
 cd -- 'bootstrap/' || return
 curl --fail --location --show-error --silent --tlsv1 -- \
-    "$BOOTSTRAP_ARTIFACT_URL" | \
+    "$BOOTSTRAP_DEP_URL" | \
     tar -f - -p --strip-components=1 -x -z
 cd -- '-' || return
