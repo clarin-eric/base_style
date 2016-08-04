efdd6855644a134cb38aa8a481d9da30a2e3730d
Sander Maijers <S.N.Maijers@gmail.com>
2016-08-04 18:08:31 +0200

    Do not extract tarball

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-mkdir -- 'bootstrap/'
-cd -- 'bootstrap/' || return
-curl --fail --location --show-error --silent --tlsv1 -- \
-    "$BOOTSTRAP_DEP_URL" | \
-    tar -f - -p --strip-components=1 -x -z
-cd -- '-' || return
+curl --fail --location --show-error --silent --tlsv1 "$BOOTSTRAP_DEP_URL" \
+    --output 'bootstrap.tgz'
