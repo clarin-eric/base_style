c0977efc8877492915a6631d8ea9b5f48d8809e1
Sander Maijers <S.N.Maijers@gmail.com>
2016-11-03 10:44:22 +0100

    Integrate `clarin-eric/www-clarin-eu_src` code
    * See https://github.com/clarin-eric/www-clarin-eu_src .
    * Update Bootstrap dep 3.3.6 -> 3.3.7.

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-
-curl --fail --location --show-error --silent --tlsv1 "$BOOTSTRAP_DEP_URL" \
-    --output 'bootstrap.tgz'
+set -e
+npm set progress='false'
+npm --silent install --global 'grunt-cli'
+curl --fail --location --proto '=https' --silent --show-error --tlsv1 \
+    "https://github.com/twbs/bootstrap/archive/v${BOOTSTRAP_VER}.zip" \
+    | bsdtar -xf -
+## Apply CLARIN base style.
+cd -- "bootstrap-${BOOTSTRAP_VER}/"
+cp -f -- '../variables.less' 'less/variables.less'
+npm --silent install
+grunt dist
+tar -f '../release.tgz' -c -v -z 'dist/'
