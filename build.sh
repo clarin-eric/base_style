2b22051dc3d1b2fbf31ed58c4ad37a5491cf9d0e
Twan Goosen <twan.goosen@mpi.nl>
2017-01-20 15:44:27 +0100

    script directory

--- build.sh
+++ build.sh
@@ -1,13 +1,1 @@
-#!/bin/sh
-set -e
-npm set progress='false'
-npm --silent install --global 'grunt-cli'
-curl --fail --location --proto '=https' --silent --show-error --tlsv1 \
-    "https://github.com/twbs/bootstrap/archive/v${BOOTSTRAP_VER}.zip" \
-    | bsdtar -xf -
-## Apply CLARIN base style.
-cd -- "bootstrap-${BOOTSTRAP_VER}/"
-cp -f -- '../src/main/less/variables.less' 'less/variables.less'
-npm --silent install
-grunt dist
-tar -f "../release-${STYLE_VERSION}.tgz" -c -v -z 'dist/'
+src/main/script/build.sh
